8096258c04af61a6ebd39fd8b28e9ac1419bd94f
MindaugasVilkas1 <mindaugas.vilkas@codeacademylt.onmicrosoft.com>
2022-03-22 12:06:16 +0200

    astunta uzduotis

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,29 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+class Calculator {
+    sum = (skaicius1, skaicius2) => {
+        let atsakymas = skaicius1 + skaicius2
+        return atsakymas
+
+    };
+    subtraction = (skaicius1, skaicius2) => {
+        let atsakymas1 = skaicius1 - skaicius2
+        return atsakymas1
+
+    };
+    multiplication = (skaicius1, skaicius2) => {
+        let atsakymas2 = skaicius1 * skaicius2
+        return atsakymas2
+    };
+    division = (skaicius1, skaicius2) => {
+        let atsakymas3 = skaicius1 / skaicius2
+        return atsakymas3
+    }
+
+}
+let Uzduotis = new Calculator();
+console.log(Uzduotis.sum(50, 100));
+console.log(Uzduotis.subtraction(100, 50));
+console.log(Uzduotis.multiplication(100, 50));
+console.log(Uzduotis.division(100, 50));
