25286181f03dd3f73d3af0b9b3c2f3b8c3ec7530
MindaugasVilkas1 <mindaugas.vilkas@codeacademylt.onmicrosoft.com>
2022-03-22 12:19:55 +0200

    devinta uzduotis

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,23 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+class Movies {
+    constructor(
+        title,
+        director,
+        budget
+    ) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+    wasExpensive = () => {
+        if (this.budget > 100000000) {
+            return true
+        } else {
+            false
+        }
+    }
+}
+let newMovie = new Movies("Avengers: Infinity War", "Joe Ruso", 236000000)
+console.log(newMovie.wasExpensive())
